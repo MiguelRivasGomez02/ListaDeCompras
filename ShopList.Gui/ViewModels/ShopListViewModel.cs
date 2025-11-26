a5ca1e959e85c0cb01425f1e05de2a5720ad3a08
Miguel Alejandro Rivas Gomez <23470281@campeche.tecnm.mx>
2025-11-26 15:08:37 -0600

    Aplicación totalmente funcional usando MVVM

--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -6,18 +6,15 @@ using System.ComponentModel;
 using System.Windows.Input;
 
 namespace ShopList.Gui.ViewModels
-
 {
-
     public partial class ShopListViewModel : ObservableObject
-
     {
-
         [ObservableProperty]
         private string _NombreDelArticulo = string.Empty;
         [ObservableProperty]
         private int _Cantidad = 1;
-
+        [ObservableProperty]
+        private ShopListItem? _elementoSeleccionado = null;
 
         public ObservableCollection<ShopListItem> ShopList { get; }
 
@@ -25,8 +22,16 @@ namespace ShopList.Gui.ViewModels
         {
             ShopList = new ObservableCollection<ShopListItem>();
             CargarDatos();
-        }
+            if (ShopList.Count > 0)
+            {
+                ElementoSeleccionado = ShopList[0];
+            }
+            else
+            {
+                ElementoSeleccionado = null;
+            }
 
+        }
         [RelayCommand]
         public void AddShopListItem()
         {
@@ -44,52 +49,63 @@ namespace ShopList.Gui.ViewModels
             };
 
             ShopList.Add(item);
+            ElementoSeleccionado = item;
             NombreDelArticulo = string.Empty;
             Cantidad = 1;
         }
+        [RelayCommand]
+        public void RemoveShopListItem()
+        {
+            if (ElementoSeleccionado == null)
+            {
+                return;
+            }
+            ShopListItem? nuevoElementoSeleccionado;
+            int indice = ShopList.IndexOf(ElementoSeleccionado);
+            if (ShopList.Count > 1)
+            {
+                if (indice == ShopList.Count - 1)
+                {
+                    nuevoElementoSeleccionado = ShopList[indice - 1];
+                }
+                else
+                {
+                    nuevoElementoSeleccionado = ShopList[indice + 1];
+                }
+            }
+            else
+            {
+                nuevoElementoSeleccionado = null;
+            }
+            ShopList.Remove(ElementoSeleccionado);
+            ElementoSeleccionado = nuevoElementoSeleccionado;
+        }
 
         private void CargarDatos()
-
         {
-
             ShopList.Add(new ShopListItem()
-
             {
-
                 Id = 1,
                 Nombre = "Pan de caja",
                 Cantidad = 1,
                 Comprado = false,
-
             });
 
             ShopList.Add(new ShopListItem()
-
             {
-
                 Id = 2,
                 Nombre = "Leche",
                 Cantidad = 3,
                 Comprado = false,
-
             });
 
             ShopList.Add(new ShopListItem()
-
             {
-
                 Id = 3,
-
                 Nombre = "Queso",
-
                 Cantidad = 100,
-
                 Comprado = false,
-
             });
-
         }
     }
 }
-
-
