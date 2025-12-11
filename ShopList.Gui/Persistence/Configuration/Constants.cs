9e21702625fa0149cd50587492b7cd113c89ba39
Miguel Alejandro Rivas Gomez <23470281@campeche.tecnm.mx>
2025-12-11 15:32:15 -0600

    Instalación de paquetes SQLite

---
+++ ShopList.Gui/Persistence/Configuration/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopList.Gui.Persistence.Configuration
+{
+    public static class Constants
+    {
+        public const string DatabaseFileName = "ShopListDb.db3";
+        public static string DatabasePath
+            => Path.Combine(
+                FileSystem.AppDataDirectory,
+                DatabaseFileName
+                );
+
+        public const SQLite.SQLiteOpenFlags Flags = 
+            SQLite.SQLiteOpenFlags.ReadWrite|
+            SQLite.SQLiteOpenFlags.Create|
+            SQLite.SQLiteOpenFlags.SharedCache;
+  
+
+    }
+}
